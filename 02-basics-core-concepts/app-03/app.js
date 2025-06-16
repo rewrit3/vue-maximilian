120bc2f56b609377d655440ef85fa381f75e9500
rewrit3 <adolfo@rewrite.com.br>
2025-06-16 13:58:08 -0400

    class: 33

--- 02-basics-core-concepts/app-03/app.js
+++ 02-basics-core-concepts/app-03/app.js
@@ -3,6 +3,8 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: "",
+      lastName: "",
+      // fullname: "",
     };
   },
   methods: {
@@ -28,12 +30,36 @@ const app = Vue.createApp({
   },
   computed: {
     fullName() {
-      if (this.name === "") {
+      if (this.name === "" || this.lastName === "") {
         return "";
       }
 
-      return this.name + " " + "Cruz";
+      return this.name + " " + this.lastName;
     },
+  },
+  watch: {
+    counter(value) {
+      if (value > 50) {
+        const that = this;
+        setTimeout(function () {
+          that.counter = 0;
+        }, 2000);
+      }
+    },
+    // name(value) {
+    //   if (value === "") {
+    //     this.fullname = "";
+    //   } else {
+    //     this.fullname = value + " " + this.lastName;
+    //   }
+    // },
+    // lastName(value) {
+    //   if (value === "") {
+    //     this.fullname = "";
+    //   } else {
+    //     this.fullname = this.name + " " + value;
+    //   }
+    // },
   },
 });
 
