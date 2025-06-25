3c8abcdd3a2a0e50e1e3a02e3534cc1f10abaf6c
rewrit3 <adolfo@rewrite.com.br>
2025-06-25 09:34:53 -0400

    class: 59

--- 04-course-project-the-monster-slayer-game/project-01/app.js
+++ 04-course-project-the-monster-slayer-game/project-01/app.js
@@ -13,10 +13,26 @@ const app = Vue.createApp({
   },
   computed: {
     monsterBarStyles() {
-      return { width: this.monsterHealth + '%' };
+      if (this.monsterHealth < 0) {
+        return {
+          width: '0%',
+        };
+      }
+
+      return {
+        width: this.monsterHealth + '%',
+      };
     },
     playerBarStyles() {
-      return { width: this.playerHealth + '%' };
+      if (this.playerHealth < 0) {
+        return {
+          width: '0%',
+        };
+      }
+
+      return {
+        width: this.playerHealth + '%',
+      };
     },
     mayUseSpecialAttack() {
       return this.currentRound % 3 !== 0;
@@ -39,6 +55,12 @@ const app = Vue.createApp({
     },
   },
   methods: {
+    startGame() {
+      this.currentRound = 0;
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.winner = null;
+    },
     attackMonster() {
       this.currentRound++;
 
@@ -72,6 +94,9 @@ const app = Vue.createApp({
       }
 
       this.attackPlayer();
+    },
+    surrender() {
+      this.winner = 'monster';
     },
   },
 });
