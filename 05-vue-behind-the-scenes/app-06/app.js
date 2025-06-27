f021c714145170a721de3b32f2a558865bddef24
rewrit3 <adolfo@rewrite.com.br>
2025-06-27 08:57:58 -0400

    class: 64

--- 05-vue-behind-the-scenes/app-06/app.js
+++ 05-vue-behind-the-scenes/app-06/app.js
@@ -16,3 +16,35 @@ const app = Vue.createApp({
 });
 
 app.mount('#app');
+
+// ...
+// let message = 'Hello';
+// let longMessage = message + ' World!';
+// console.log(longMessage);
+
+// message = 'Hello!!!';
+// console.log(longMessage);
+
+const data = {
+  message: 'Hello',
+  longMessage: 'Hello World!',
+};
+
+const handler = {
+  set(target, key, value) {
+    // console.log(target);
+    // console.log(key);
+    // console.log(value);
+
+    if (key === 'message') {
+      target.longMessage = value + ' World!!!';
+    }
+
+    target.message = value;
+  },
+};
+
+const proxy = new Proxy(data, handler);
+proxy.message = 'Hello!!!';
+
+console.log(proxy.longMessage);
