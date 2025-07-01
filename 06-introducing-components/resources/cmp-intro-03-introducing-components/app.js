e1ca52da78d62a3dc27a185f980e0b42facfbf36
rewrit3 <adolfo@rewrite.com.br>
2025-07-01 14:30:07 -0400

    class: 74

--- 06-introducing-components/resources/cmp-intro-03-introducing-components/app.js
+++ 06-introducing-components/resources/cmp-intro-03-introducing-components/app.js
@@ -3,6 +3,12 @@ const app = Vue.createApp({
     return {
       friends: [
         {
+          id: 'manuel',
+          name: 'Manuel Lorenz',
+          phone: '01234 5678 991',
+          email: 'manuel@localhost.com',
+        },
+        {
           id: 'julie',
           name: 'Julie Jones',
           phone: '09876 543 221',
@@ -15,23 +21,23 @@ const app = Vue.createApp({
 
 app.component('friend-contact', {
   template: `
-    <li>
-      <h2>{{ friend.name }}</h2>
-      <button @click="toggleDetails()">
-        {{ detailsAreVisible ? 'Hide' : 'Show' }} Details
-      </button>
-      <ul v-if="detailsAreVisible">
-        <li><strong>Phone:</strong> {{ friend.phone }}</li>
-        <li><strong>Email:</strong> {{ friend.email }}</li>
-      </ul>
-    </li>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails()">
+      {{ detailsAreVisible ? 'Hide' : 'Show' }} Details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
   `,
   data() {
     return {
       detailsAreVisible: false,
       friend: {
         id: 'manuel',
-        name: 'Manuel Lorenz',
+        name: 'Manuel Lorenzo',
         phone: '01234 5678 991',
         email: 'manuel@localhost.com',
       },
