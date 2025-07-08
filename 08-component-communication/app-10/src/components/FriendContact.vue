038dd498710b3adb2ca5faa28d5d4008ebb19d65
rewrit3 <adolfo@rewrite.com.br>
2025-07-08 13:55:55 -0400

    class: 96

--- 08-component-communication/app-10/src/components/FriendContact.vue
+++ 08-component-communication/app-10/src/components/FriendContact.vue
@@ -1,9 +1,10 @@
 <script>
 export default {
-  props: ['name', 'phoneNumber', 'emailAddress'],
+  props: ['name', 'phoneNumber', 'emailAddress', 'isFavorite'],
   data() {
     return {
       detailsAreVisible: false,
+      friendIsFavorite: this.isFavorite,
       friend: {
         id: 'manuel',
         name: 'Manuel Lorenz',
@@ -16,17 +17,25 @@ export default {
     toggleDetails() {
       this.detailsAreVisible = !this.detailsAreVisible;
     },
+    toggleFavorite() {
+      if (this.friendIsFavorite === '1') {
+        this.friendIsFavorite = '0';
+      } else {
+        this.friendIsFavorite = '1';
+      }
+    },
   },
 };
 </script>
 
 <template>
   <li>
-    <h2>{{ name }}</h2>
+    <h2>{{ name }} {{ friendIsFavorite === '1' ? '(Favorite)' : '' }}</h2>
 
-    <button @click="toggleDetails">
+    <button @click="toggleDetails" style="margin-right: 1rem">
       {{ detailsAreVisible ? 'Hide' : 'Show' }} Details
     </button>
+    <button @click="toggleFavorite">Toggle Favorite</button>
 
     <ul v-if="detailsAreVisible">
       <li><strong>Phone:</strong> {{ phoneNumber }}</li>
