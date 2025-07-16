0b56e61e364a5237cd7966bc1b351d3fa09f17a9
rewrit3 <adolfo@rewrite.com.br>
2025-07-16 09:29:28 -0400

    class: 101

--- 08-component-communication/app-10/src/components/FriendContact.vue
+++ 08-component-communication/app-10/src/components/FriendContact.vue
@@ -27,6 +27,18 @@ export default {
       // },
     },
   },
+  emits: ['toggle-favorite'],
+  // emits: {
+  //   'toggle-favorite': function (id) {
+  //     if (id) {
+  //       return true;
+  //     } else {
+  //       console.warn('ID is missing!');
+
+  //       return false;
+  //     }
+  //   },
+  // },
   data() {
     return {
       detailsAreVisible: false,
