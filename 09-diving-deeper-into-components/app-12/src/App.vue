df0e050818faf54107b2ea23005fd37d974ad7b6
rewrit3 <adolfo@rewrite.com.br>
2025-08-05 08:58:22 -0400

    class: 113

--- 09-diving-deeper-into-components/app-12/src/App.vue
+++ 09-diving-deeper-into-components/app-12/src/App.vue
@@ -5,13 +5,21 @@
     <user-info
       :full-name="activeUser.name"
       :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
+      :role="activeUser.role"></user-info>
   </div>
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
+
 export default {
+  components: {
+    TheHeader,
+    BadgeList,
+    UserInfo,
+  },
   data() {
     return {
       activeUser: {
