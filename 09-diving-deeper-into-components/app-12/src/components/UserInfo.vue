432697f64c754be3a967aad8e63791382d2fb207
rewrit3 <adolfo@rewrite.com.br>
2025-08-06 09:04:44 -0400

    class: 115

--- 09-diving-deeper-into-components/app-12/src/components/UserInfo.vue
+++ 09-diving-deeper-into-components/app-12/src/components/UserInfo.vue
@@ -1,11 +1,11 @@
 <template>
-  <section>
+  <base-card>
     <header>
       <h3>{{ fullName }}</h3>
       <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
     </header>
     <p>{{ infoText }}</p>
-  </section>
+  </base-card>
 </template>
 
 <script>
@@ -15,14 +15,6 @@ export default {
 </script>
 
 <style scoped>
-section {
-  margin: 2rem auto;
-  max-width: 30rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
-}
-
 section header {
   display: flex;
   justify-content: space-between;
