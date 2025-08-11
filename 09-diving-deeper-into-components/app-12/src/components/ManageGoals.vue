c78ef1784c1894c4d5f5dd2738b4e46cf7f45ffe
rewrit3 <adolfo@rewrite.com.br>
2025-08-11 08:51:27 -0400

    class: 123

--- 09-diving-deeper-into-components/app-12/src/components/ManageGoals.vue
+++ 09-diving-deeper-into-components/app-12/src/components/ManageGoals.vue
@@ -5,12 +5,14 @@
     <input type="text" ref="goal" />
     <button @click="setGoal">Set Goal</button>
 
-    <error-alert v-if="inputIsInvalid">
-      <h2>Input is invalid!</h2>
-      <p>Please enter at least a few characters.</p>
+    <teleport to="body">
+      <error-alert v-if="inputIsInvalid">
+        <h2>Input is invalid!</h2>
+        <p>Please enter at least a few characters.</p>
 
-      <button @click="confirmError">Okay</button>
-    </error-alert>
+        <button @click="confirmError">Okay</button>
+      </error-alert>
+    </teleport>
   </div>
 </template>
 
