7d4191bc14064b13bf9df19531488378f2da3bef
rewrit3 <adolfo@rewrite.com.br>
2025-08-12 14:24:25 -0400

    class: 132

--- 10-course-project-the-learning-resources-app/project-02/src/App.vue
+++ 10-course-project-the-learning-resources-app/project-02/src/App.vue
@@ -1,22 +1,13 @@
 <template>
-  <ul>
-    <LearningResource
-      v-for="resource in storedResources"
-      :key="resource.id"
-      :title="resource.title"
-      :description="resource.description"
-      :link="resource.link"
-    >
-    </LearningResource>
-  </ul>
+  <StoredResources :resources="storedResources"></StoredResources>
 </template>
 
 <script>
-import LearningResource from './components/learning-resources/LearningResource.vue';
+import StoredResources from './components/learning-resources/StoredResources.vue';
 
 export default {
   components: {
-    LearningResource,
+    StoredResources,
   },
   data() {
     return {
@@ -38,3 +29,19 @@ export default {
   },
 };
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
