972e060d171c86e34e23f26c584fb80f81a2ae82
rewrit3 <adolfo@rewrite.com.br>
2025-08-12 14:06:03 -0400

    class: 131

--- 10-course-project-the-learning-resources-app/project-02/src/App.vue
+++ 10-course-project-the-learning-resources-app/project-02/src/App.vue
@@ -1,13 +1,23 @@
 <template>
   <ul>
-    <li v-for="resource in storedResources" :key="resource.id">
-      {{ resource }}
-    </li>
+    <LearningResource
+      v-for="resource in storedResources"
+      :key="resource.id"
+      :title="resource.title"
+      :description="resource.description"
+      :link="resource.link"
+    >
+    </LearningResource>
   </ul>
 </template>
 
 <script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
+
 export default {
+  components: {
+    LearningResource,
+  },
   data() {
     return {
       storedResources: [
