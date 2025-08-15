3295a3c42a5255c2d0ea1912749fb316add9af4b
rewrit3 <adolfo@rewrite.com.br>
2025-08-15 09:16:33 -0400

    class: 141

--- 10-course-project-the-learning-resources-app/project-02/src/components/ui/BaseDialog.vue
+++ 10-course-project-the-learning-resources-app/project-02/src/components/ui/BaseDialog.vue
@@ -1,23 +1,25 @@
 <template>
-  <div @click="$emit('close')"></div>
+  <teleport to="body">
+    <div @click="$emit('close')"></div>
 
-  <dialog open>
-    <header>
-      <slot name="header">
-        <h2>{{ title }}</h2>
-      </slot>
-    </header>
+    <dialog open>
+      <header>
+        <slot name="header">
+          <h2>{{ title }}</h2>
+        </slot>
+      </header>
 
-    <section>
-      <slot></slot>
-    </section>
+      <section>
+        <slot></slot>
+      </section>
 
-    <menu>
-      <slot name="actions">
-        <base-button @click="$emit('close')">Close</base-button>
-      </slot>
-    </menu>
-  </dialog>
+      <menu>
+        <slot name="actions">
+          <base-button @click="$emit('close')">Close</base-button>
+        </slot>
+      </menu>
+    </dialog>
+  </teleport>
 </template>
 
 <script>
