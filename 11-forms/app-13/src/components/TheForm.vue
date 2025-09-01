5523f4ca4a92d56e26d5529ea8f039c6ed32853c
rewrit3 <adolfo@rewrite.com.br>
2025-09-01 09:15:02 -0400

    class: 150

--- 11-forms/app-13/src/components/TheForm.vue
+++ 11-forms/app-13/src/components/TheForm.vue
@@ -99,7 +99,7 @@
       </div>
     </div>
     <div class="form-control">
-      <rating-control></rating-control>
+      <rating-control v-model="rating"></rating-control>
     </div>
     <div class="form-control">
       <input
@@ -131,6 +131,7 @@ export default {
       interest: [],
       how: null,
       confirm: false,
+      rating: null,
       userNameValidity: 'pending',
     };
   },
@@ -153,6 +154,9 @@ export default {
 
       console.log('Confirm: ' + this.confirm);
       this.confirm = false;
+
+      console.log('Rating: ' + this.rating);
+      this.rating = false;
     },
     validateInput() {
       if (this.userName === '') {
