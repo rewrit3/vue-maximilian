a2cb506a6e8cb0e1b59d6793b6609047142a65b9
rewrit3 <adolfo@rewrite.com.br>
2025-09-17 09:46:24 -0400

    class: 180

--- 13-routing/app-15/src/main.js
+++ 13-routing/app-15/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
+import NotFound from './components/nav/NotFound.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
@@ -9,9 +10,11 @@ import UsersList from './components/users/UsersList.vue';
 const router = createRouter({
   history: createWebHistory(),
   routes: [
+    { path: '/', redirect: '/teams' },
     { path: '/teams', component: TeamsList },
     { path: '/teams/:teamId', component: TeamMembers, props: true },
     { path: '/users', component: UsersList },
+    { path: '/:notFound(.*)', component: NotFound },
   ],
   linkActiveClass: 'active',
 });
