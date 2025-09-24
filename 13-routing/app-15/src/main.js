4a6ab907e69e9564b98a8830cb7a6c26193f12ba
rewrit3 <adolfo@rewrite.com.br>
2025-09-24 09:41:27 -0400

    class: 190

--- 13-routing/app-15/src/main.js
+++ 13-routing/app-15/src/main.js
@@ -16,6 +16,7 @@ const router = createRouter({
     {
       name: 'teams',
       path: '/teams',
+      meta: { needsAuth: true },
       components: { default: TeamsList, footer: TeamsFooter },
       children: [
         {
@@ -57,13 +58,19 @@ router.beforeEach(function (to, from, next) {
   console.log('Global beforeEach');
   console.log(to, from);
 
+  if (to.meta.needsAuth) {
+    console.log('Needs auth.');
+
+    next();
+  } else {
+    next();
+  }
+
   // if (to.name === 'team-members') {
   //   next();
   // } else {
   //   next({ name: 'team-members', params: { teamId: 't2' } });
   // }
-
-  next();
 });
 
 router.afterEach(function (to, from) {
