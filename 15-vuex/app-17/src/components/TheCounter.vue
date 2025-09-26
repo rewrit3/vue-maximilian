ab6da894c8d1c37539c0caad140f77b720e741f2
rewrit3 <adolfo@rewrite.com.br>
2025-09-26 09:04:41 -0400

    class: 216

---
+++ 15-vuex/app-17/src/components/TheCounter.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  computed: {
+    counter() {
+      return this.$store.state.counter;
+    },
+  },
+};
+</script>
+
+<template>
+  <h3>{{ counter }}</h3>
+</template>
